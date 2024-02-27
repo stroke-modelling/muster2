750482db1fce19981dc592e35a57f04c45a7c3a2
Michael Allen <michael.allen1966@gmail.com>
2024-02-27 10:41:32 +0000

    Add model class

---
+++ classes/model.py
@@ -0,0 +1,21 @@
+"""
+Model class
+"""
+ 
+class Model(object):
+    """
+    Model class
+    """
+
+    def __init__(self, scenario, geodata):
+        """
+        Constructor class for Model.
+        """
+
+        # Scenario
+        self.scenario = scenario
+
+        # Geodata
+        self.geodata = geodata
+
+        
